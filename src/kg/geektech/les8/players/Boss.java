3933dffe676dde17fcde03c2d96e69b83f5ab898
KENCHku <kench.mkk3@gmail.com>
2020-10-01 17:20:39 +0600

    Initial commit

---
+++ src/kg/geektech/les8/players/Boss.java
@@ -0,0 +1,9 @@
+package kg.geektech.les8.players;
+
+import kg.geektech.les8.players.GameEntity;
+
+public class Boss extends GameEntity {
+    public Boss(int health, int damage) {
+        super(health, damage);
+    }
+}
